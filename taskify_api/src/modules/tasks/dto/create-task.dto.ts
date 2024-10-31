778e51e5ccc0fd535638762c678759cfc5012b9a
christian-lopezxd <00179320@uca.edu.sv>
2024-10-31 09:20:09 -0600

    :memo: Add: Insomnia file

--- taskify_api/src/modules/tasks/dto/create-task.dto.ts
+++ taskify_api/src/modules/tasks/dto/create-task.dto.ts
@@ -1,5 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTaskDto {
+  @ApiProperty({
+    example: 'Complete the project report',
+    description: 'The title of the task',
+  })
   title: string;
+
+  @ApiProperty({
+    example: 'Finish the final report for the project by end of the week.',
+    description: 'A brief description of the task',
+    required: false, // Indica que es opcional
+  })
   description?: string;
-  userId: number; // Relación con el usuario
+
+  @ApiProperty({
+    example: 1,
+    description: 'The ID of the user associated with this task',
+  })
+  userId: number;
 }
--- taskify_api/src/modules/users/users.service.ts
+++ taskify_api/src/modules/users/users.service.ts
@@ -7,23 +7,33 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
     return this.prisma.user.create({ data });
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
+  async findAll() {
+    return this.prisma.user.findMany({
+      include: { tasks: true },
+    });
   }
 
-  findOne(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: { tasks: true }, 
+    });
   }
 
-  update(id: number, data: UpdateUserDto) {
-    return this.prisma.user.update({ where: { id }, data });
+  async update(id: number, data: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
   }
 
-  remove(id: number) {
-    return this.prisma.user.delete({ where: { id } });
+  async remove(id: number) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
